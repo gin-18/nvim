b1a7f1c672c8ea81f2b0b192105fe06186927736
gin <gin-18@qq.com>
2024-11-09 10:55:05 +0800

    feat: add enable and disable format in conform

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -60,11 +60,35 @@ return {
         javascript = { 'prettier' },
         vue = { 'prettier' },
       },
-      format_on_save = {
-        timeout_ms = 600,
-        lsp_fallback = true,
-      },
     },
+    config = function()
+      require('conform').setup({
+        format_on_save = function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = 'fallback' }
+        end,
+      })
+
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+    end,
   },
 
   -- comment
