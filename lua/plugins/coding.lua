bed08c0a39168e9537bf6392c5d0ff2e0a302e2a
gin <gin-18@qq.com>
2025-03-29 08:31:34 +0800

    feat: add linting

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -187,6 +187,37 @@ return {
     end,
   },
 
+  -- linting
+  {
+    'mfussenegger/nvim-lint',
+    event = {
+      'BufReadPre',
+      'BufNewFile',
+    },
+    config = function()
+      local lint = require('lint')
+      lint.linters_by_ft = {
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        javascriptreact = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
+        vue = { 'eslint_d' },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set('n', '<leader>lt', function()
+        lint.try_lint()
+      end, { desc = 'Trigger linting for current file' })
+    end,
+  },
+
   -- formatting
   {
     'stevearc/conform.nvim',
@@ -196,7 +227,13 @@ return {
         formatters_by_ft = {
           lua = { 'stylua' },
           javascript = { 'prettier' },
+          typescript = { 'prettier' },
           vue = { 'prettier' },
+          css = { 'prettier' },
+          html = { 'prettier' },
+          json = { 'prettier' },
+          yaml = { 'prettier' },
+          toml = { 'prettier' },
         },
 
         format_on_save = function(bufnr)
