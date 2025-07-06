9d6a601e9059a957f56e8a937c715efbbac62b52
gin <gin-18@qq.com>
2025-07-06 13:00:34 +0800

    refactor(coding): remove lsp and completion setting

--- init.vim
+++ init.vim
@@ -1,54 +1,5 @@
-"  ██████   █████                   █████   █████  ███
-" ░░██████ ░░███                   ░░███   ░░███  ░░░
-"  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████
-"  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███
-"  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███
-"  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███
-"  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████
-" ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░
-
 "   @Author: gin
 
-" -----------------------------------------------------------------
-"                                                                 -
-" plugin config for which written in vimscript                    -
-"                                                                 -
-" -----------------------------------------------------------------
-" markdown-preview
-let g:mkdp_theme = 'dark'
-let g:mkdp_auto_start = 0
-let g:mkdp_refresh_slow = 1
-let g:mkdp_markdown_css = expand('~/.config/nvim/static/markdown.css')
-let g:mkdp_highlight_css = expand('~/.config/nvim/static/highlight.css')
-
-" table-mode
-noremap <M-T> <cmd>TableModeToggle<cr>
-noremap <M-R> <cmd>TableModeRealign<cr>
-
-function! s:isAtStartOfLine(mapping)
-  let text_before_cursor = getline('.')[0 : col('.')-1]
-  let mapping_pattern = '\V' . escape(a:mapping, '\')
-  let comment_pattern = '\V' . escape(substitute(&l:commentstring, '%s.*$', '', ''), '\')
-  return (text_before_cursor =~? '^' . ('\v(' . comment_pattern . '\v)?') . '\s*\v' . mapping_pattern . '\v$')
-endfunction
-
-inoreabbrev <expr> <bar><bar>
-      \ <SID>isAtStartOfLine('\|\|') ?
-      \ '<c-o>:TableModeEnable<cr><bar><space><bar><left><left>' : '<bar><bar>'
-inoreabbrev <expr> __
-      \ <SID>isAtStartOfLine('__') ?
-      \ '<c-o>:silent! TableModeDisable<cr>' : '__'
-
-" md-img-paste
-let g:mdip_imgdir = 'images'
-
-autocmd FileType markdown nmap <buffer><silent> <space>pi :call mdip#MarkdownClipboardImage()<CR>
-
-" -----------------------------------------------------------------
-"                                                                 -
-" load plugin config in lua/plugins                               -
-"                                                                 -
-" -----------------------------------------------------------------
 lua << EOF
 require('core.disabled')
 require('core.options')
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,123 +1,52 @@
 return {
-  -- snippet
-  {
-    'L3MON4D3/LuaSnip',
-    version = 'v2.*',
-    build = 'make install_jsregexp',
-    event = 'InsertEnter',
-    dependencies = {
-      {
-        { 'saadparwaiz1/cmp_luasnip' },
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
-    },
-    keys = {
-      {
-        '<C-j>',
-        function()
-          return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<space>'
-        end,
-        expr = true,
-        silent = true,
-        mode = 'i',
-      },
-      {
-        '<C-j>',
-        function()
-          require('luasnip').jump(1)
-        end,
-        mode = 's',
-      },
-      {
-        '<C-k>',
-        function()
-          require('luasnip').jump(-1)
-        end,
-        mode = { 'i', 's' },
-      },
-    },
-  },
-
-  -- completion
-  -- NOTE: get warning in formatting
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-cmdline',
-      'onsails/lspkind.nvim',
-    },
-    config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      local lspkind = require('lspkind')
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
-          { name = 'treesitter' },
-          { name = 'path' },
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol',
-            maxwidth = 50,
-            ellipsis_char = '...',
-            show_labelDetails = true,
-          }),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-l>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-        }),
-      })
-
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' },
-        },
-      })
-
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' },
-          { name = 'cmdline' },
-        }),
-      })
-    end,
-  },
-
   -- ai
   {
     'Exafunction/codeium.vim',
     event = 'BufEnter',
   },
+
+  -- lsp installation
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = {
+      {
+        'williamboman/mason.nvim',
+        cmd = 'Mason',
+        dependencies = {
+          'WhoIsSethDaniel/mason-tool-installer.nvim',
+          opts = {
+            ensure_installed = {
+              'stylua',
+              'prettier',
+              'eslint_d',
+            },
+          },
+        },
+        opts = {
+          ui = {
+            border = 'rounded',
+            width = 0.8,
+            height = 0.7,
+            icons = {
+              package_installed = '󰺧',
+              package_pending = '',
+              package_uninstalled = '󰺭',
+            },
+          },
+        },
+      },
+    },
+    opts = {
+      ensure_installed = {
+        'vimls',
+        'lua_ls',
+        'vtsls',
+        'vue_ls',
+      },
+    },
+  },
+
+  -- completion
 
   -- git
   {
@@ -184,37 +113,6 @@ return {
     },
     config = function(_, opts)
       require('nvim-treesitter.configs').setup(opts)
-    end,
-  },
-
-  -- linting
-  {
-    'mfussenegger/nvim-lint',
-    event = {
-      'BufReadPre',
-      'BufNewFile',
-    },
-    config = function()
-      local lint = require('lint')
-      lint.linters_by_ft = {
-        javascript = { 'eslint_d' },
-        typescript = { 'eslint_d' },
-        javascriptreact = { 'eslint_d' },
-        typescriptreact = { 'eslint_d' },
-        vue = { 'eslint_d' },
-      }
-
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          pcall(require, lint.try_lint())
-        end,
-      })
-
-      vim.keymap.set('n', '<leader>lt', function()
-        lint.try_lint()
-      end, { desc = 'Trigger linting for current file' })
     end,
   },
 
@@ -287,13 +185,12 @@ return {
     config = true,
   },
   {
-    'windwp/nvim-ts-autotag',
+    'norcalli/nvim-colorizer.lua',
     event = 'VeryLazy',
     config = true,
   },
   {
-    'norcalli/nvim-colorizer.lua',
-    event = 'VeryLazy',
-    config = true,
+    'j-hui/fidget.nvim',
+    opts = {},
   },
 }
